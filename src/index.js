9db710aa2bbda96321d5ca23d9333d6a73444d2c
Icaro Ferreira <icaro.ferreira.filho@gmail.com>
2022-02-12 09:09:51 -0300

    atualizado layout da pagina de presentes. adicoinado dependencia. corrigido bug de transporte de variavel pra view

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import express from 'express'
+import express from "express";
 import * as exprhbs from "express-handlebars";
-
+const {
+    allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+const handlebars = require("handlebars");
 const app = express();
 const routes = require("./routes");
 
@@ -9,7 +12,14 @@ app.use(express.json());
 app.use(express.static("src/public"));
 app.use(routes);
 
-app.engine("handlebars", exprhbs.engine());
+app.engine(
+    "handlebars",
+    exprhbs.engine({
+        defaultLayout: "main",
+        handlebars: allowInsecurePrototypeAccess(handlebars),
+        layoutsDir: __dirname + "/views/layouts/",
+    })
+);
 app.set("view engine", "handlebars");
 app.set("views", "src/views");
 
